c58844ea1ad82f8c85477c9a4b29e1fe82196134
Danny Vu <danny.hvu@gmail.com>
2019-06-05 11:42:37 +0200

    Env var

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-if (!process.env.NODE_ENV === 'production') {
-  const dotEnvResult = require('dotenv').config();
-  if (dotEnvResult.error) {
-    throw dotEnvResult.error;
-  }
+const dotEnvResult = require('dotenv').config();
+
+if (dotEnvResult.error) {
+  throw dotEnvResult.error;
 }
 
 module.exports = {
