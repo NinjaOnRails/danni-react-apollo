52f3e9620a6e1e2ddeac0fedfeb1d2dd588fb220
Danny Vu <danny.hvu@gmail.com>
2019-06-11 15:03:41 +0200

    Hardcode OGM details

--- components/Watch.js
+++ components/Watch.js
@@ -158,10 +158,19 @@ class Watch extends Component {
                     <title>Danni | {titleVi}</title>
                     <meta
                       property="og:url"
-                      content={'http://danni.tv/watch?id=' + id}
+                      content={
+                        'http://danni.tv/watch?id=' +
+                        'cjwg6v6i9001t0701qjpduo1m'
+                      }
                     />
-                    <meta property="og:title" content={titleVi} />
-                    <meta property="og:image" content={originThumbnailUrl} />
+                    <meta
+                      property="og:title"
+                      content="EU Thực Sự Dân Chủ Như Thế Nào?"
+                    />
+                    <meta
+                      property="og:image"
+                      content="https://i.ytimg.com/vi/h4Uu5eyN6VU/mqdefault.jpg"
+                    />
                     <meta property="og:locale" content="vi_VN" />
                   </Head>
                   <div>
