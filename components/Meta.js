d157c7f1749358424070be23ad65b016845fdcea
Danny Vu <danny.hvu@gmail.com>
2019-06-18 12:56:02 +0200

    Implement GA

--- components/Meta.js
+++ components/Meta.js
@@ -2,6 +2,22 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${
+        process.env.GA_TRACKING_ID
+      }`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GA_TRACKING_ID}');
+          `,
+      }}
+    />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <link rel="shortcut icon" href="/static/favicon.png" />
