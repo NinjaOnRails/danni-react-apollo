9d4f76365eb4e160781fa692bbdab53ab9bd342b
Danny Vu <danny.hvu@gmail.com>
2019-08-11 17:13:40 +0200

    Fix withApollo, cookie passing works, SSR broken

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import withApollo from '../lib/withApollo';
+// import withApollo from '../lib/withApolloClient';
 import Page from '../components/Page';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
+
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
