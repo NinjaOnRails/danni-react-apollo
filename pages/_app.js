100e393d3fd10c506ad776f75cdab0bedee3767a
Danny Vu <danny.hvu@gmail.com>
2019-08-13 11:42:17 +0200

    Make SSR and cookies work with GraphQL post error

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,27 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
-import withApollo from '../lib/withApollo';
-// import withApollo from '../lib/withApolloClient';
+// import withApollo from '../lib/withApollo';
+import withApollo from '../lib/withApolloClient';
 import Page from '../components/Page';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component, pageProps, apolloClient } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
+        <ApolloProvider client={apollo}>
           <Page>
             <Component {...pageProps} />
           </Page>
@@ -20,5 +30,21 @@ class MyApp extends App {
     );
   }
 }
+
+// class MyApp extends App {
+//   render() {
+//     const { Component, pageProps, apolloClient } = this.props;
+
+//     return (
+//       <Container>
+//         <ApolloProvider client={apolloClient}>
+//           <Page>
+//             <Component {...pageProps} />
+//           </Page>
+//         </ApolloProvider>
+//       </Container>
+//     );
+//   }
+// }
 
 export default withApollo(MyApp);
