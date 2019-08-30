d7a157bfc253469c1d50cf2b7592fe15ae613e45
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-08-30 13:39:30 +0200

    comments seed

--- components/Comment/CommentList.js
+++ components/Comment/CommentList.js
@@ -2,95 +2,11 @@ import React from 'react';
 import { Button, Comment, Form } from 'semantic-ui-react';
 import VideoComment from './Comment';
 import { CommentSectionStyles } from '../styles/Commentstyles';
-
-const commentData = [
-  {
-    id: 415134,
-    author: 'Matt',
-    date: 'Today at 5:42PM',
-    text: 'How artistic!',
-    avatar: '',
-    likes: 20,
-    reply: [],
-  },
-  {
-    id: 37489237589,
-    author: 'Elliot Fu',
-    date: 'Yesterday at 12:30AM',
-    text: 'This has been very useful for my research. Thanks as well!',
-    avatar: '',
-    likes: 20,
-    reply: [
-      {
-        id: 324345,
-        author: 'Jenny Hess',
-        date: 'Just now',
-        text: 'Elliot you are always so right :)',
-        avatar: '',
-        likes: 20,
-      },
-      {
-        id: 5654654,
-        author: 'Matt',
-        date: 'Today at 5:42PM',
-        text: 'How artistic!',
-        avatar: '',
-        likes: 20,
-        reply: [],
-      },
-      {
-        id: 7984165,
-        author: 'Matt',
-        date: 'Today at 5:42PM',
-        text: 'How artistic!',
-        avatar: '',
-        likes: 20,
-        reply: [],
-      },
-      {
-        id: 4651657,
-        author: 'Matt',
-        date: 'Today at 5:42PM',
-        text: 'How artistic!',
-        avatar: '',
-        likes: 20,
-        reply: [],
-      },
-      {
-        id: 468546357,
-        author: 'Matt',
-        date: 'Today at 5:42PM',
-        text: 'How artistic!',
-        avatar: '',
-        likes: 20,
-        reply: [],
-      },
-    ],
-  },
-  {
-    id: 4654327,
-    author: 'Joe Henderson',
-    date: '5 days ago',
-    text: 'Dude, this is awesome. Thanks so much',
-    avatar: '',
-    likes: 20,
-    reply: [],
-  },
-  {
-    id: 454657,
-    author: 'Joe Henderson',
-    date: '5 days ago',
-    text:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, blanditiis. Voluptate laboriosam quidem quis perferendis consectetur minima, dolore voluptatem rerum, error magni veritatis quae. Officia incidunt iure soluta ad nostrum.',
-    avatar: '',
-    likes: 20,
-    reply: [],
-  },
-];
+import commentSeedData from './commentSeedData';
 
 class CommentList extends React.Component {
   renderComments = () => {
-    return commentData.map(comment => (
+    return commentSeedData.map(comment => (
       <VideoComment key={comment.id} comment={comment} />
     ));
   };
