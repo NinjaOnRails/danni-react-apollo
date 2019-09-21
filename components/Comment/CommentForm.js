74c74f2931072d081ac00167d11c9371ca97c291
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-09-21 13:51:39 +0200

    CommentForm and CommentList splitted

---
+++ components/Comment/CommentForm.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { Button, Form, Loader } from 'semantic-ui-react';
+import { Mutation } from 'react-apollo';
+import { adopt } from 'react-adopt';
+import PropTypes from 'prop-types';
+import PleaseSignIn from '../Authentication/PleaseSignIn';
+import { CREATE_COMMENT_MUTATION } from '../../graphql/mutation';
+import { VIDEO_COMMENTS_QUERY } from '../../graphql/query';
+import { user } from '../UI/ContentLanguage';
+import Error from '../UI/ErrorMessage';
+
+/* eslint-disable */
+const createCommentMutation = ({ commentInput, videoId, render }) => (
+  <Mutation
+    mutation={CREATE_COMMENT_MUTATION}
+    variables={{
+      video: videoId,
+      text: commentInput,
+    }}
+    refetchQueries={[
+      {
+        query: VIDEO_COMMENTS_QUERY,
+        variables: { video: videoId },
+      },
+    ]}
+  >
+    {(createComment, createCommentResult) => {
+      return render({ createComment, createCommentResult });
+    }}
+  </Mutation>
+);
+/* eslint-enable */
+
+const Composed = adopt({
+  user,
+  createCommentMutation,
+});
+
+class CommentForm extends React.Component {
+  state = {
+    commentInput: '',
+    commentInputValid: false,
+  };
+
+  onTextChange = ({ target: { value } }) => {
+    this.setState({ commentInput: value, commentInputValid: value.length > 0 });
+  };
+
+  onCommentSubmit = async createComment => {
+    const { data } = await createComment();
+    if (data) this.setState({ commentInput: '', commentInputValid: false });
+  };
+
+  renderCommentForm = (createCommentLoading, createComment) => {
+    const { commentInput, commentInputValid } = this.state;
+    const { client, hideSigninToComment } = this.props;
+    return (
+      <PleaseSignIn action="Comment" minimalistic hidden={hideSigninToComment}>
+        <Form
+          loading={createCommentLoading}
+          reply
+          onSubmit={() => {
+            if (commentInput.length > 0) this.onCommentSubmit(createComment);
+          }}
+        >
+          <Form.TextArea
+            placeholder="Write a comment..."
+            onChange={this.onTextChange}
+            value={commentInput}
+            onClick={() =>
+              client.writeData({
+                data: { hideSigninToComment: false },
+              })
+            }
+          />
+          <Button content="Add Comment" primary disabled={!commentInputValid} />
+        </Form>
+      </PleaseSignIn>
+    );
+  };
+
+  render() {
+    const { videoId, commentsLoading } = this.props;
+    const { commentInput } = this.state;
+    return (
+      <Composed videoId={videoId} commentInput={commentInput}>
+        {({
+          createCommentMutation: {
+            createComment,
+            createCommentResult: {
+              error: createCommentError,
+              loading: createCommentLoading,
+            },
+          },
+        }) => (
+          <>
+            <Error error={createCommentError} />
+            {commentsLoading ? (
+              <Loader active inline="centered" />
+            ) : (
+              this.renderCommentForm(createCommentLoading, createComment)
+            )}
+          </>
+        )}
+      </Composed>
+    );
+  }
+}
+
+CommentForm.defaultProps = {
+  commentsLoading: null,
+  hideSigninToComment: true,
+};
+
+CommentForm.propTypes = {
+  videoId: PropTypes.string.isRequired,
+  client: PropTypes.object.isRequired,
+  commentsLoading: PropTypes.bool,
+  hideSigninToComment: PropTypes.bool,
+};
+
+export default CommentForm;
--- components/Comment/CommentSection.js
+++ components/Comment/CommentSection.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import { Button, Comment, Form, Loader } from 'semantic-ui-react';
+import { Comment, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { Mutation, Query } from 'react-apollo';
+import { Query } from 'react-apollo';
 import { adopt } from 'react-adopt';
-import VideoComment from './Comment';
 import CommentSectionStyles from '../styles/Commentstyles';
 import Error from '../UI/ErrorMessage';
-import { CREATE_COMMENT_MUTATION } from '../../graphql/mutation';
 import { VIDEO_COMMENTS_QUERY } from '../../graphql/query';
-import PleaseSignIn from '../Authentication/PleaseSignIn';
 import CommentList from './CommentList';
 import { user } from '../UI/ContentLanguage';
-
+import CommentForm from './CommentForm';
 /* eslint-disable */
-const createCommentMutation = ({ commentInput, videoId, render }) => (
-  <Mutation
-    mutation={CREATE_COMMENT_MUTATION}
-    variables={{
-      video: videoId,
-      text: commentInput,
-    }}
-    refetchQueries={[
-      {
-        query: VIDEO_COMMENTS_QUERY,
-        variables: { video: videoId },
-      },
-    ]}
-  >
-    {(createComment, createCommentResult) => {
-      return render({ createComment, createCommentResult });
-    }}
-  </Mutation>
-);
-
 const videoComments = ({ videoId, render }) => (
   <Query query={VIDEO_COMMENTS_QUERY} variables={{ video: videoId }}>
     {/* {videoComments => {
@@ -45,63 +22,21 @@ const videoComments = ({ videoId, render }) => (
 
 const Composed = adopt({
   user,
-  createCommentMutation,
   videoComments,
 });
 
 class CommentSection extends React.Component {
-  state = {
-    commentInput: '',
-    commentInputValid: false,
-  };
-
-  onTextChange = ({ target: { value } }) => {
-    this.setState({ commentInput: value, commentInputValid: value.length > 0 });
-  };
-
-  onCommentSubmit = async createComment => {
-    const { data } = await createComment();
-    if (data) this.setState({ commentInput: '', commentInputValid: false });
-  };
-
-  renderComments = (data, createCommentLoading, createComment, currentUser) => {
+  renderComments = (data, currentUser, commentsLoading) => {
     const { videoId, client } = this.props;
-    const { commentInput, commentInputValid } = this.state;
-
     return (
       <CommentSectionStyles>
-        {console.log('CommentSection')}
         <Comment.Group size="large">
-          <PleaseSignIn
-            action="Comment"
-            minimalistic
-            hidden={data.hideSigninToComment}
-          >
-            <Form
-              loading={createCommentLoading}
-              reply
-              onSubmit={() => {
-                if (commentInput.length > 0)
-                  this.onCommentSubmit(createComment);
-              }}
-            >
-              <Form.TextArea
-                placeholder="Write a comment..."
-                onChange={this.onTextChange}
-                value={commentInput}
-                onClick={() =>
-                  client.writeData({
-                    data: { hideSigninToComment: false },
-                  })
-                }
-              />
-              <Button
-                content="Add Comment"
-                primary
-                disabled={!commentInputValid}
-              />
-            </Form>
-          </PleaseSignIn>
+          <CommentForm
+            videoId={videoId}
+            client={client}
+            commentsLoading={commentsLoading}
+            hideSigninToComment={data.hideSigninToComment}
+          />
           {data.comments && (
             <CommentList
               comments={data.comments}
@@ -116,19 +51,11 @@ class CommentSection extends React.Component {
   };
 
   render() {
-    const { commentInput } = this.state;
     const { videoId } = this.props;
     return (
-      <Composed videoId={videoId} commentInput={commentInput}>
+      <Composed videoId={videoId}>
         {({
           user: { currentUser },
-          createCommentMutation: {
-            createComment,
-            createCommentResult: {
-              error: createCommentError,
-              loading: createCommentLoading,
-            },
-          },
           videoComments: {
             error: commentsLoadingError,
             loading: commentsLoading,
@@ -137,16 +64,10 @@ class CommentSection extends React.Component {
         }) => (
           <>
             <Error error={commentsLoadingError} />
-            <Error error={createCommentError} />
             {commentsLoading ? (
               <Loader active inline="centered" />
             ) : (
-              this.renderComments(
-                data,
-                createCommentLoading,
-                createComment,
-                currentUser
-              )
+              this.renderComments(data, currentUser, commentsLoading)
             )}
           </>
         )}
