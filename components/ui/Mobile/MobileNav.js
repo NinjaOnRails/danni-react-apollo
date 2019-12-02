69e7c3229290cdd4865c81aaae9ef26b42243749
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-12-02 23:23:45 +0100

    Iphone 5 sizing fix and active item removed

--- components/ui/Mobile/MobileNav.js
+++ components/ui/Mobile/MobileNav.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 import { Mutation } from 'react-apollo';
 import Link from 'next/link';
@@ -6,8 +7,8 @@ import DrawerToggle from './DrawerToggle';
 import { TOGGLE_SIDEDRAWER_MUTATION } from '../../../graphql/mutation';
 
 const StyledNav = styled.div`
-  overflow: hidden;
-  /* background-color: #333; */
+  /* overflow: hidden;
+  background-color: white;
   position: fixed;
   bottom: 0;
   left: 0;
@@ -15,11 +16,21 @@ const StyledNav = styled.div`
   height: 48px;
   width: 100%;
   z-index: 2;
-  border-top: 1px solid grey;
+  border-top: 1px solid grey; */
   .ui.menu {
     width: 100%;
     display: flex;
     justify-content: space-around;
+    overflow: hidden;
+    background-color: white;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 48px;
+    width: 100%;
+    z-index: 2;
+    border-top: 1px solid grey;
   }
   .ui.labeled.icon.menu .item {
     margin: auto;
@@ -33,6 +44,12 @@ const StyledNav = styled.div`
   @media (min-width: 639px) {
     display: none;
   }
+  @media (max-width: 321px) {
+    .ui.menu .item {
+      padding-left: 0;
+      padding-right: 0;
+    }
+  }
 `;
 
 const sidebarItems = [
@@ -43,14 +60,18 @@ const sidebarItems = [
 ];
 
 const MobileNav = () => {
-  const activeItem = 'home';
+  const [activeItem, setActiveItem] = useState('home');
 
   return (
     <StyledNav>
       <Menu icon="labeled" borderless>
         {sidebarItems.map(({ linkName, link, icon, miniIcon }) => (
           <Link href={link} key={icon}>
-            <Menu.Item name={icon} active={activeItem === icon}>
+            <Menu.Item
+              name={icon}
+              // active={activeItem === icon}
+              // onClick={() => setActiveItem(icon)}
+            >
               <Icon.Group size="large">
                 <Icon name={icon} />
                 {miniIcon && <Icon color="grey" name="plus" size="tiny" />}
