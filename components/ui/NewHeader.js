013898fffd2dbcb5506e774f5c48cd007907ef5b
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-12-29 14:15:11 +0100

    Add video button replaced by auth buttons

--- components/ui/NewHeader.js
+++ components/ui/NewHeader.js
@@ -18,6 +18,28 @@ const StyledHeader = styled.header`
   box-sizing: border-box;
   position: relative;
 
+  ul {
+    padding-inline-start: 20px;
+    display: flex;
+    list-style: none;
+    .ui.button,
+    .ui.buttons .button,
+    .ui.buttons .or {
+      font-size: 15px;
+    }
+    li {
+      margin-left: auto;
+      padding: 10px 20px;
+    }
+  }
+
+  .ui.basic.red.button,
+  .ui.basic.red.buttons .button,
+  .ui.basic.green.button,
+  .ui.basic.green.buttons .button {
+    color: black;
+  }
+
   .bar {
     display: flex;
     justify-content: space-between;
@@ -26,95 +48,90 @@ const StyledHeader = styled.header`
     position: relative;
   }
 
-  .nav {
+  .nav,
+  .tools {
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 5px;
 
-    ul {
-      display: flex;
-      li {
-        margin-left: auto;
-        padding: 10px 20px;
-        list-style: none;
-        &:first-child {
-          margin-left: 20px;
-        }
-
-        a,
-        button {
-          margin: 0;
-          padding: 0;
-          border: 0;
-          display: flex;
-          align-items: center;
-          text-align: center;
-          line-height: normal;
-          position: relative;
-          background: none;
-          border: 0;
-          cursor: pointer;
-          color: ${props => props.theme.black};
-
-          &:after {
-            height: 2px;
-            background: red;
-            content: '';
-            width: 0;
-            position: absolute;
-            transform: translateX(-50%);
-            transition-property: width;
-            transition-duration: 0.4s;
-            transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
-            -webkit-transform: translateX(-50%);
-            -webkit-transition-property: width;
-            -webkit-transition-duration: 0.4s;
-            -webkit-transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
-            transform-style: preserve-3d;
-            backface-visibility: hidden;
-            -webkit-transform-style: preserve-3d;
-            -webkit-backface-visibility: hidden;
-            left: 50%;
-            margin-top: 2rem;
-          }
-
-          &:hover,
-          &:focus {
-            outline: none;
-            &:after {
-              width: 100%;
-            }
-          }
-        }
+    .auth-button {
+      background-color: #21ba45 ;
+      /* border: 1px solid rgba(255, 0, 0, 0.5);
+      margin: -1px 0; */
+      outline: 0px;
+      border-radius: 3px;
+      /* font-weight: bold; */
+      align-items: center;
+      text-align: center;
+      justify-content: center;
+      &:first-child {
+        margin-right: 15px;
+      }
+      &:hover {
+        border-color: rgba(255, 0, 0);
       }
 
-      .signup {
-        border: 1px solid rgba(255, 0, 0, 0.5);
-        margin: -1px 0;
-        outline: 0px;
-        border-radius: 3px;
-        font-weight: bold;
-        align-items: center;
-        text-align: center;
-        justify-content: center;
+      a {
+        color: #fff;
 
-        &:hover {
-          border-color: rgba(255, 0, 0);
+        &:after {
+          content: none;
         }
+      }
+      /* line-height: 5px; */
+    }
 
-        a {
-          &:after {
-            content: none;
-          }
+    .signin {
+      background-color: #e1e1e1;
+      /* border-color: #e1e1e1; */
+    }
+
+    a,
+    button {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      display: flex;
+      align-items: center;
+      text-align: center;
+      line-height: normal;
+      position: relative;
+      background: none;
+      border: 0;
+      cursor: pointer;
+      color: ${props => props.theme.black};
+
+      &:after {
+        height: 2px;
+        background: red;
+        content: '';
+        width: 0;
+        position: absolute;
+        transform: translateX(-50%);
+        transition-property: width;
+        transition-duration: 0.4s;
+        transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
+        -webkit-transform: translateX(-50%);
+        -webkit-transition-property: width;
+        -webkit-transition-duration: 0.4s;
+        -webkit-transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
+        transform-style: preserve-3d;
+        backface-visibility: hidden;
+        -webkit-transform-style: preserve-3d;
+        -webkit-backface-visibility: hidden;
+        left: 50%;
+        margin-top: 2rem;
+      }
+
+      &:hover,
+      &:focus {
+        outline: none;
+        &:after {
+          width: 100%;
         }
-        /* line-height: 5px; */
       }
     }
-  }
-
-  .tools {
-    display: flex;
   }
 
   @media (max-width: 639px) {
@@ -141,7 +158,6 @@ Router.onRouteChangeError = () => {
 const Header = () => (
   <StyledHeader>
     <Logo />
-
     <User>
       {({ data }) => {
         const currentUser = data ? data.currentUser : null;
@@ -154,13 +170,15 @@ const Header = () => (
                     <a>Trang chủ</a>
                   </Link>
                 </li>
-
                 <li>
                   <Link href="/about">
                     <a>Giới thiệu</a>
                   </Link>
                 </li>
-
+              </ul>
+            </div>
+            <div className="tools">
+              <ul>
                 {currentUser ? (
                   <>
                     <li>
@@ -174,27 +192,21 @@ const Header = () => (
                   </>
                 ) : (
                   <>
-                    <li>
+                    <li className="auth-button signin">
                       <Link href="/signin">
-                        <a>Đăng Nhập</a>
+                        Đăng Nhập
+                        {/* <a>Đăng Nhập</a> */}
                       </Link>
                     </li>
-                    <li className="signup">
+                    <li className="auth-button">
                       <Link href="/signup">
-                        <a>Đăng Ký</a>
+                        Đăng Ký
+                        {/* <a>Đăng Ký</a> */}
                       </Link>
                     </li>
                   </>
                 )}
               </ul>
-            </div>
-            <div className="tools">
-              {/* <Search /> */}
-              <Link href="/new">
-                <Button primary size="huge">
-                  <Icon name="plus" /> Thêm Video
-                </Button>
-              </Link>
             </div>
           </div>
         );
@@ -204,3 +216,14 @@ const Header = () => (
 );
 
 export default Header;
+
+{
+  /* <Search /> */
+}
+{
+  /* <Link href="/new">
+<Button primary size="huge">
+<Icon name="plus" /> Thêm Video
+</Button>
+</Link> */
+}
