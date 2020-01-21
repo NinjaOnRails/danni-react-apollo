066dfa727e229ff10615a92f64da8c55850bd64b
Danny Vu <danny.hvu@gmail.com>
2020-01-21 23:09:49 +0100

    Small typo fix

--- components/About.js
+++ components/About.js
@@ -19,37 +19,37 @@ const About = () => (
       <title key="title">Danni TV - Giới thiệu</title>
       <meta key="metaTitle" name="title" content="Danni TV - Giới thiệu" />
     </Head>
-    <AboutStyle>
+    <AboutStyles>
       <Container text>
         <Header as="h2" textAlign="center">
-              🌍 MỤC ĐÍCH CỦA CHÚNG TÔI 🌍
-            </Header>
-            <p>Phổ biến hóa kiến ​​thức toàn cầu.</p>
-            <Header as="h2" textAlign="center">
-              💪 CHÚNG TÔI LÀM GÌ 💪
-            </Header>
-            <p>
-              Cộng đồng của chúng tôi tìm hiểu, dịch và lồng tiếng cho các video
-              trực tuyến sang các ngôn ngữ khác. Chúng tôi tập trung vào các nội
-              dung có các yếu tố liên quan tới giáo dục, phát triển bản thân và
-              chuyên môn, giải trí thông minh, với mong muốn chia sẻ những góc
-              nhìn sâu sắc hơn về các vấn đề quan trọng trong xã hội. Thông qua
-              hoạt động này, chúng tôi hy vọng nội dung của các video thú vị sẽ
-              vượt qua được các giới hạn về ngôn ngữ và không gian và tới được
-              với đông đảo khán giả.
-            </p>
-            <Header as="h2" textAlign="center">
-              🚀 KÊU GỌI HÀNH ĐỘNG 🚀
-            </Header>
-            <p>
-              Hãy cùng nhau vượt qua các rào cản ngôn ngữ để mọi người có thể
-              truy cập thư viện video tốt nhất của Internet.
-            </p>
-            <p>
-              Các ngôn ngữ hiện đang được sử dụng: tiếng Séc, tiếng Anh và tiếng
-              Việt.
-            </p>
-            {/* <Header as="h2">Join us!</Header>
+          🌍 MỤC ĐÍCH CỦA CHÚNG TÔI 🌍
+        </Header>
+        <p>Phổ biến hóa kiến ​​thức toàn cầu.</p>
+        <Header as="h2" textAlign="center">
+          💪 CHÚNG TÔI LÀM GÌ 💪
+        </Header>
+        <p>
+          Cộng đồng của chúng tôi tìm hiểu, dịch và lồng tiếng cho các video
+          trực tuyến sang các ngôn ngữ khác. Chúng tôi tập trung vào các nội
+          dung có các yếu tố liên quan tới giáo dục, phát triển bản thân và
+          chuyên môn, giải trí thông minh, với mong muốn chia sẻ những góc nhìn
+          sâu sắc hơn về các vấn đề quan trọng trong xã hội. Thông qua hoạt động
+          này, chúng tôi hy vọng nội dung của các video thú vị sẽ vượt qua được
+          các giới hạn về ngôn ngữ và không gian và tới được với đông đảo khán
+          giả.
+        </p>
+        <Header as="h2" textAlign="center">
+          🚀 KÊU GỌI HÀNH ĐỘNG 🚀
+        </Header>
+        <p>
+          Hãy cùng nhau vượt qua các rào cản ngôn ngữ để mọi người có thể truy
+          cập thư viện video tốt nhất của Internet.
+        </p>
+        <p>
+          Các ngôn ngữ hiện đang được sử dụng: tiếng Séc, tiếng Anh và tiếng
+          Việt.
+        </p>
+        {/* <Header as="h2">Join us!</Header>
             <p>
               BECOME A DUBBER Register and start dubbing. Use your phone or
               another device to start recording. Then just upload your audio
@@ -69,13 +69,13 @@ const About = () => (
               us feedback, write about us Mention us in your videos, blogs or
               posts. Join the team!
             </p> */}
-            <Header as="h2" textAlign="center">
-              🤙 Liên lạc 🤙
-            </Header>
-            <p>info.dannitv@gmail.com</p>
-            <p>(+420) 774897789</p>
+        <Header as="h2" textAlign="center">
+          🤙 Liên lạc 🤙
+        </Header>
+        <p>info.dannitv@gmail.com</p>
+        <p>(+420) 774897789</p>
       </Container>
-    </AboutStyle>
+    </AboutStyles>
   </>
 );
 
