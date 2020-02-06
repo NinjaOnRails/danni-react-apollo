a24d5aadd9bd267a79da69730428705211521d7c
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2020-02-06 16:51:35 +0100

    7 indent for VideoInfo

--- components/Video/VideoInfo.js
+++ components/Video/VideoInfo.js
@@ -132,73 +132,74 @@ const VideoInfo = ({
   return (
     <>
       <>
-        {() => (
-          <VideoInfoStyles>
-            <div className="basic-info">
-              <Header>
-                <h1>{title}</h1>
-              </Header>
-              {currentUser && currentUser.id === addedBy.id && (
-                <div className="buttons">
-                  <Link
-                    href={{
-                      pathname: '/edit',
-                      query,
-                    }}
-                  >
-                    <Button icon labelPosition="left">
-                      <Icon name="write" />
-                      Sửa
-                    </Button>
-                  </Link>
-                  <VideoDeleteButton
-                    id={id}
-                    audioId={audioId}
-                    title={title}
-                    userId={currentUser.id}
-                    contentLanguage={contentLanguage}
-                    redirect
-                  />
-                </div>
-              )}
-              <div className="views-social">
-                <div className="vid-statistic">
-                  <YoutubeViews originId={originId} />
-                  <Statistic size="mini" horizontal>
-                    <Statistic.Value>{upVoteCount} </Statistic.Value>
-                    <Statistic.Label>
-                      <Icon
-                        id="UPVOTE"
-                        name="thumbs up"
-                        link
-                        color={
-                          userVoteType && userVoteType.type === 'UPVOTE'
-                            ? 'green'
-                            : 'black'
-                        }
-                        size="large"
-                        onClick={e => onVideoLike(e.target.id)}
-                      />
-                    </Statistic.Label>
-                  </Statistic>
-                  <Statistic size="mini" horizontal>
-                    <Statistic.Value>{downVoteCount}</Statistic.Value>
-                    <Statistic.Label>
-                      <Icon
-                        id="DOWNVOTE"
-                        name="thumbs down"
-                        link
-                        color={
-                          userVoteType && userVoteType.type === 'DOWNVOTE'
-                            ? 'red'
-                            : 'black'
-                        }
-                        size="large"
-                        onClick={e => onVideoLike(e.target.id)}
-                      />
-                    </Statistic.Label>
-                  </Statistic>
-                  {/* <Statistic size="mini" horizontal>
+        <>
+          {() => (
+            <VideoInfoStyles>
+              <div className="basic-info">
+                <Header>
+                  <h1>{title}</h1>
+                </Header>
+                {currentUser && currentUser.id === addedBy.id && (
+                  <div className="buttons">
+                    <Link
+                      href={{
+                        pathname: '/edit',
+                        query,
+                      }}
+                    >
+                      <Button icon labelPosition="left">
+                        <Icon name="write" />
+                        Sửa
+                      </Button>
+                    </Link>
+                    <VideoDeleteButton
+                      id={id}
+                      audioId={audioId}
+                      title={title}
+                      userId={currentUser.id}
+                      contentLanguage={contentLanguage}
+                      redirect
+                    />
+                  </div>
+                )}
+                <div className="views-social">
+                  <div className="vid-statistic">
+                    <YoutubeViews originId={originId} />
+                    <Statistic size="mini" horizontal>
+                      <Statistic.Value>{upVoteCount} </Statistic.Value>
+                      <Statistic.Label>
+                        <Icon
+                          id="UPVOTE"
+                          name="thumbs up"
+                          link
+                          color={
+                            userVoteType && userVoteType.type === 'UPVOTE'
+                              ? 'green'
+                              : 'black'
+                          }
+                          size="large"
+                          onClick={e => onVideoLike(e.target.id)}
+                        />
+                      </Statistic.Label>
+                    </Statistic>
+                    <Statistic size="mini" horizontal>
+                      <Statistic.Value>{downVoteCount}</Statistic.Value>
+                      <Statistic.Label>
+                        <Icon
+                          id="DOWNVOTE"
+                          name="thumbs down"
+                          link
+                          color={
+                            userVoteType && userVoteType.type === 'DOWNVOTE'
+                              ? 'red'
+                              : 'black'
+                          }
+                          size="large"
+                          onClick={e => onVideoLike(e.target.id)}
+                        />
+                      </Statistic.Label>
+                    </Statistic>
+                    {/* <Statistic size="mini" horizontal>
      <Statistic.Value>
        {audioId ? audio[0].comment.length : comment.length}
      </Statistic.Value>
@@ -206,78 +207,79 @@ const VideoInfo = ({
        <Icon name="comment" size="large" />
      </Statistic.Label>
    </Statistic> */}
-                </div>
-                <div>
-                  <FacebookShareButton className="fb-share-button" url={url}>
-                    <FacebookIcon size={32} round />
-                  </FacebookShareButton>
+                  </div>
+                  <div>
+                    <FacebookShareButton className="fb-share-button" url={url}>
+                      <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                  </div>
                 </div>
               </div>
-            </div>
-            <Segment>
-              <Header>
-                <h2>Tác giả: {originAuthor}</h2>
-              </Header>
-              {(audio[0] && (
+              <Segment>
                 <Header>
-                  <h3>
-                    <Link
-                      href={{
-                        pathname: '/user',
-                        query: { id: audio[0].author.id },
-                      }}
-                    >
-                      <a>
-                        <Image avatar src={audio[0].author.avatar} />
-                        {audio[0].author
-                          ? audio[0].author.displayName
-                          : 'deleted user'}
-                      </a>
-                    </Link>
-                  </h3>
+                  <h2>Tác giả: {originAuthor}</h2>
                 </Header>
-              )) || (
-                <Header>
-                  <h3>
-                    <Link
-                      href={{
-                        pathname: '/user',
-                        query: { id: addedBy.id },
-                      }}
-                    >
-                      <a>
-                        <Image avatar src={addedBy.avatar} />
-                        {addedBy ? addedBy.displayName : 'deleted user'}
-                      </a>
-                    </Link>
-                  </h3>
-                </Header>
-              )}
-              <div
-                ref={descriptionDiv}
-                className={
-                  showFullDescription
-                    ? 'description'
-                    : `description description-preview`
-                }
-              >
-                {(audio[0] && audio[0].description && (
-                  <>{audio[0].description}</>
-                )) ||
-                  (originDescription && <>{originDescription}</>)}
-              </div>
-              {descriptionOverflow && (
-                <button
-                  type="button"
-                  onClick={() => toggleFullDescription()}
-                  className="ui button"
+                {(audio[0] && (
+                  <Header>
+                    <h3>
+                      <Link
+                        href={{
+                          pathname: '/user',
+                          query: { id: audio[0].author.id },
+                        }}
+                      >
+                        <a>
+                          <Image avatar src={audio[0].author.avatar} />
+                          {audio[0].author
+                            ? audio[0].author.displayName
+                            : 'deleted user'}
+                        </a>
+                      </Link>
+                    </h3>
+                  </Header>
+                )) || (
+                  <Header>
+                    <h3>
+                      <Link
+                        href={{
+                          pathname: '/user',
+                          query: { id: addedBy.id },
+                        }}
+                      >
+                        <a>
+                          <Image avatar src={addedBy.avatar} />
+                          {addedBy ? addedBy.displayName : 'deleted user'}
+                        </a>
+                      </Link>
+                    </h3>
+                  </Header>
+                )}
+                <div
+                  ref={descriptionDiv}
+                  className={
+                    showFullDescription
+                      ? 'description'
+                      : `description description-preview`
+                  }
                 >
-                  {showFullDescription ? 'Đóng' : 'Tiếp'}
-                </button>
-              )}
-            </Segment>
-          </VideoInfoStyles>
-        )}
+                  {(audio[0] && audio[0].description && (
+                    <>{audio[0].description}</>
+                  )) ||
+                    (originDescription && <>{originDescription}</>)}
+                </div>
+                {descriptionOverflow && (
+                  <button
+                    type="button"
+                    onClick={() => toggleFullDescription()}
+                    className="ui button"
+                  >
+                    {showFullDescription ? 'Đóng' : 'Tiếp'}
+                  </button>
+                )}
+              </Segment>
+            </VideoInfoStyles>
+          )}
+        </>
       </>
     </>
   );
