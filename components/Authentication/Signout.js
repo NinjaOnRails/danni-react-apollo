daa5b4e16d06d0caeb98eddc0649d3d0af5a442e
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2020-02-12 14:25:56 +0100

    onSignOut moved

--- components/Authentication/Signout.js
+++ components/Authentication/Signout.js
@@ -1,6 +1,14 @@
 import { ApolloConsumer } from 'react-apollo';
 import { useSignoutMutation } from './authHooks';
-import { onSignout } from './utils';
+import { CURRENT_USER_QUERY } from '../../graphql/query';
+
+const onSignout = async ({ signout, client }) => {
+  await signout();
+  localStorage.clear();
+  await client.resetStore();
+  if (FB && FB.getAccessToken()) FB.logout();
+  return client.query({ query: CURRENT_USER_QUERY });
+};
 
 const Signout = () => {
   const [signout] = useSignoutMutation();
