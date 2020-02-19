13a34529e5f3ba9711a2b55182dee169d54a94bc
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2020-02-19 12:23:56 +0100

    NewUserInfoForm removed, imports fixed

--- components/Authentication/UserInfoForm.js
+++ components/Authentication/UserInfoForm.js
@@ -1,237 +1,198 @@
-import React, { Component } from 'react';
-import { Checkbox, Item, Form, Button, Icon, Popup } from 'semantic-ui-react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Mutation } from 'react-apollo';
-import { UPDATE_USER_MUTATION } from '../../graphql/mutation';
-import { USER_QUERY, CURRENT_USER_QUERY } from '../../graphql/query';
+import { Checkbox, Item, Form, Button, Icon, Popup } from 'semantic-ui-react';
 import Error from '../UI/ErrorMessage';
+import { userInfoFields, userPasswordFields } from './fieldTypes';
+import { inputChangeHandler, clearForm } from './utils';
+import { useUpdateUserMutation } from './authHooks';
 
-class UserInfoForm extends Component {
-  state = {
-    password: '',
-    newPassword: '',
-    confirmPassword: '',
-    showPasswordChange: false,
-    displayPassword: false,
-  };
+const UserInfoForm = ({ currentUser, onCancelClick }) => {
+  const [userPasswordForm, setUserPasswordForm] = useState({
+    ...userPasswordFields,
+  });
+  const [userInfoForm, setUserInfoForm] = useState({ ...userInfoFields });
+  const [infoVisibility, setInfoVisibility] = useState({});
 
-  handleChange = (e, { type, name, value, checked }) => {
-    const val = type === 'checkbox' ? checked : value;
-    this.setState({ [name]: val });
-  };
+  const [displayPassword, setDisplayPassword] = useState(false);
+  const [showPasswordChange, setShowPasswordChange] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(null);
 
-  onSubmit = async (e, updateUser) => {
-    e.preventDefault();
-    const { data } = await updateUser();
-    if (data) this.props.onCancelClick();
-  };
-
-  render() {
-    const { currentUser, onCancelClick } = this.props;
-    const {
-      password,
-      newPassword,
-      confirmPassword,
-      showPasswordChange,
-      displayPassword,
-    } = this.state;
-    const {
-      id,
-      displayName,
-      name,
-      showName,
-      bio,
-      showBio,
-      email,
-      showEmail,
-      location,
-      showLocation,
-    } = currentUser;
-    return (
-      <Mutation
-        mutation={UPDATE_USER_MUTATION}
-        variables={this.state}
-        refetchQueries={[
-          {
-            query: USER_QUERY,
-            variables: {
-              id,
-            },
-          },
-          {
-            query: CURRENT_USER_QUERY,
-          },
-        ]}
-      >
-        {(updateUser, { loading, error }) => (
-          <Item.Content verticalAlign="middle">
-            <Form
-              size="large"
-              unstackable
-              loading={loading}
-              onSubmit={e => this.onSubmit(e, updateUser)}
-            >
-              <Form.Group inline>
-                <Form.Input
-                  label="Display name"
-                  required
-                  type="text"
-                  id="displayName"
-                  name="displayName"
-                  defaultValue={displayName}
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group inline>
-                <Form.Input
-                  label="Name"
-                  type="text"
-                  id="name"
-                  name="name"
-                  defaultValue={name}
-                  onChange={this.handleChange}
-                />
-                <Checkbox
-                  label="Public"
-                  name="showName"
-                  defaultChecked={showName}
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group inline>
-                <Form.Input
-                  label="Info"
-                  type="text"
-                  id="bio"
-                  name="bio"
-                  defaultValue={bio}
-                  onChange={this.handleChange}
-                />
-                <Checkbox
-                  label="Public"
-                  name="showBio"
-                  defaultChecked={showBio}
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group inline>
-                <Form.Input
-                  label="Location"
-                  type="text"
-                  id="location"
-                  name="location"
-                  defaultValue={location}
-                  onChange={this.handleChange}
-                />
-                <Checkbox
-                  label="Public"
-                  name="showLocation"
-                  defaultChecked={showLocation}
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group inline>
-                <Form.Input
-                  label="E-mail"
-                  required
-                  type="email"
-                  id="email"
-                  name="email"
-                  defaultValue={email}
-                  onChange={this.handleChange}
-                />
-                <Checkbox
-                  label="Public"
-                  name="showEmail"
-                  defaultChecked={showEmail}
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              {(showPasswordChange && (
-                <>
-                  <Form.Group inline>
-                    <Form.Input
-                      label="Old password"
-                      type={displayPassword ? 'text' : 'password'}
-                      id="password"
-                      name="password"
-                      value={password}
-                      onChange={this.handleChange}
-                    />
-                    <Popup
-                      content={
-                        displayPassword ? 'Hide password' : 'Show password'
-                      }
-                      trigger={
-                        <Icon
-                          className="display-hide-password"
-                          name={displayPassword ? 'eye' : 'eye slash'}
-                          onClick={() =>
-                            this.setState({ displayPassword: !displayPassword })
-                          }
-                        />
-                      }
-                    />
-                  </Form.Group>
-                  <Form.Group inline>
-                    <Form.Input
-                      label="New password"
-                      type={displayPassword ? 'text' : 'password'}
-                      id="newPassword"
-                      name="newPassword"
-                      value={newPassword}
-                      onChange={this.handleChange}
-                      autoComplete="new-password"
-                    />
-                  </Form.Group>
-                  <Form.Group inline>
-                    <Form.Input
-                      label="Confirm password"
-                      type={displayPassword ? 'text' : 'password'}
-                      id="confirmPassword"
-                      name="confirmPassword"
-                      value={confirmPassword}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                </>
-              )) || (
-                <p>
-                  <Button
-                    onClick={() => this.setState({ showPasswordChange: true })}
-                  >
-                    Change password
-                  </Button>
-                </p>
-              )}
-              <Error error={error} />
-              <Button
-                type="submit"
-                primary
-                icon
-                labelPosition="left"
-                size="big"
-              >
-                <Icon name="check" />
-                Submit{loading && 'ting'}
-              </Button>
-              <Button
-                type="button"
-                icon
-                labelPosition="left"
-                size="big"
-                onClick={onCancelClick}
-              >
-                <Icon name="cancel" />
-                Cancel
-              </Button>
-            </Form>
-          </Item.Content>
-        )}
-      </Mutation>
-    );
+  const { id } = currentUser;
+  const variables = {};
+  const formElArr = [];
+  const formPassArr = [];
+  if (showPasswordChange) {
+    Object.keys(userPasswordForm).forEach(key => {
+      variables[key] = userPasswordForm[key].value;
+      formPassArr.push({
+        id: key,
+        input: userPasswordForm[key],
+      });
+    });
   }
-}
+  Object.keys(userInfoForm).forEach(key => {
+    const { boxName } = userInfoForm[key];
+
+    if (boxName) {
+      variables[boxName] = infoVisibility[boxName];
+    }
+    variables[key] = userInfoForm[key].value;
+
+    formElArr.push({
+      id: key,
+      input: userInfoForm[key],
+    });
+  });
+
+  const [updateUser, { loading, error }] = useUpdateUserMutation({
+    id,
+    variables,
+  });
+
+  const handleChange = (e, { name, checked }) => {
+    setInfoVisibility({ ...infoVisibility, [name]: checked });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const { newPassword, confirmPassword } = userPasswordForm;
+    if (newPassword.value !== confirmPassword.value) {
+      clearForm(userPasswordFields, setUserPasswordForm);
+      setPasswordsMatch({
+        message: 'Mật khẩu mới không khớp. Xin vui lòng điền lại',
+      });
+    } else {
+      setPasswordsMatch(null);
+      const { data } = await updateUser();
+      if (data) onCancelClick();
+    }
+  };
+
+  return (
+    <Item.Content verticalAlign="middle">
+      <Form
+        size="large"
+        unstackable
+        loading={loading}
+        onSubmit={e => onSubmit(e)}
+      >
+        <Error error={error} />
+        <Error error={passwordsMatch} />
+        {formElArr.map(
+          ({
+            id: inputId,
+            input: {
+              boxName,
+              modified,
+              validation,
+              valid,
+              inputConfig: { label, type, name },
+            },
+          }) => (
+            <Form.Group inline key={inputId}>
+              <Form.Input
+                required={name === 'displayName' || name === 'email'}
+                label={label}
+                type={type}
+                id={name}
+                name={name}
+                defaultValue={currentUser[name]}
+                error={validation && !valid && modified}
+                onChange={e =>
+                  inputChangeHandler(e, inputId, userInfoForm, setUserInfoForm)
+                }
+              />
+              {boxName && (
+                <Checkbox
+                  label="Public"
+                  name={boxName}
+                  defaultChecked={currentUser[boxName]}
+                  onChange={handleChange}
+                />
+              )}
+            </Form.Group>
+          )
+        )}
+        {(showPasswordChange &&
+          formPassArr.map(
+            ({
+              id: inputId,
+              input: {
+                value,
+                modified,
+                validation,
+                valid,
+                mustMatch,
+                inputConfig: { label, type, name },
+              },
+            }) => (
+              <Form.Group inline key={inputId}>
+                <Form.Input
+                  autoComplete="new-password"
+                  label={label}
+                  type={displayPassword ? 'text' : type}
+                  id={name}
+                  name={name}
+                  value={value}
+                  error={
+                    (validation && !valid && modified) ||
+                    (mustMatch && passwordsMatch && !modified)
+                  }
+                  onChange={e =>
+                    inputChangeHandler(
+                      e,
+                      inputId,
+                      userPasswordForm,
+                      setUserPasswordForm
+                    )
+                  }
+                />
+                {name === 'password' && (
+                  <Popup
+                    content={
+                      displayPassword ? 'Hide password' : 'Show password'
+                    }
+                    trigger={
+                      <Icon
+                        className="display-hide-password"
+                        name={displayPassword ? 'eye' : 'eye slash'}
+                        onClick={() => setDisplayPassword(!displayPassword)}
+                      />
+                    }
+                  />
+                )}
+              </Form.Group>
+            )
+          )) || (
+          <p>
+            <Button
+              onClick={() => {
+                setShowPasswordChange(true);
+              }}
+            >
+              Change password
+            </Button>
+          </p>
+        )}
+
+        <Button type="submit" primary icon labelPosition="left" size="big">
+          <Icon name="check" />
+          Submit{loading && 'ting'}
+        </Button>
+        <Button
+          type="button"
+          icon
+          labelPosition="left"
+          size="big"
+          onClick={onCancelClick}
+        >
+          <Icon name="cancel" />
+          Cancel
+        </Button>
+      </Form>
+    </Item.Content>
+  );
+};
 
 UserInfoForm.propTypes = {
   currentUser: PropTypes.object,
