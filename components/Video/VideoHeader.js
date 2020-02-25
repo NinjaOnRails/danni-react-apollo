5aead6e5ddf3c0db3a0294f86654d66c5ff2b9bb
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2020-02-25 12:50:21 +0100

    VideoHeader changes reverted

--- components/Video/VideoHeader.js
+++ components/Video/VideoHeader.js
@@ -1,65 +1,68 @@
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 
-const VideoHeader = ({
-  video: {
-    audio,
-    originAuthor,
-    originTitle,
-    originThumbnailUrl,
-    originThumbnailUrlSd,
-    originLanguage,
-    originDescription,
-  },
-  url,
-}) => (
-  <Head>
-    <title key="title">
-      {audio[0] ? audio[0].title : originTitle} | Danni TV
-    </title>
-    <meta name="author" content={originAuthor} />
-    <meta
-      key="metaTitle"
-      name="title"
-      content={audio[0] ? audio[0].title : originTitle}
-    />
-    <meta
-      key="description"
-      name="description"
-      content={
-        audio[0] && audio[0].description
-          ? audio[0].description
-          : originDescription
-      }
-    />
-    <meta
-      key="og:image"
-      property="og:image"
-      content={
-        audio[0] && audio[0].customThumbnail
-          ? audio[0].customThumbnail
-          : originThumbnailUrlSd || originThumbnailUrl
-      }
-    />
-    <meta property="og:type" content="article" key="og:type" />
-    <meta property="og:url" content={url} key="og:url" />
-    <meta
-      property="og:title"
-      content={audio[0] ? audio[0].title : originTitle}
-      key="og:title"
-    />
-    <meta property="og:locale" content={originLanguage || ''} />
-    <meta
-      key="og:description"
-      property="og:description"
-      content={
-        audio[0] && audio[0].description
-          ? audio[0].description
-          : originDescription
-      }
-    />
-  </Head>
-);
+const VideoHeader = props => {
+  const {
+    video: {
+      audio,
+      originAuthor,
+      originTitle,
+      originThumbnailUrl,
+      originThumbnailUrlSd,
+      originLanguage,
+      originDescription,
+    },
+    url,
+  } = props;
+  return (
+    <Head>
+      <title key="title">
+        {audio[0] ? audio[0].title : originTitle} | Danni TV
+      </title>
+      <meta name="author" content={originAuthor} />
+      <meta
+        key="metaTitle"
+        name="title"
+        content={audio[0] ? audio[0].title : originTitle}
+      />
+      <meta
+        key="description"
+        name="description"
+        content={
+          audio[0] && audio[0].description
+            ? audio[0].description
+            : originDescription
+        }
+      />
+      <meta
+        key="og:image"
+        property="og:image"
+        content={
+          audio[0] && audio[0].customThumbnail
+            ? audio[0].customThumbnail
+            : originThumbnailUrlSd || originThumbnailUrl
+        }
+      />
+      <meta property="og:type" content="article" key="og:type" />
+      <meta property="og:url" content={url} key="og:url" />
+      <meta
+        property="og:title"
+        content={audio[0] ? audio[0].title : originTitle}
+        key="og:title"
+      />
+      <meta property="og:locale" content={originLanguage || ''} />
+      <meta
+        key="og:description"
+        property="og:description"
+        content={
+          audio[0] && audio[0].description
+            ? audio[0].description
+            : originDescription
+        }
+      />
+    </Head>
+  );
+};
 
 VideoHeader.propTypes = {
   video: PropTypes.object.isRequired,
