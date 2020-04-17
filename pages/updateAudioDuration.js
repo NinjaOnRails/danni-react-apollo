00b96bb7b052a3af93a3f5509cf207a8a6c66a76
Danny Vu <danny.hvu@gmail.com>
2020-04-17 20:10:21 +0200

    Set custom backend domain endpoint

--- pages/updateAudioDuration.js
+++
@@ -1,5 +0,0 @@
-import UpdateAudioDuration from '../components/Video/UpdateAudioDuration';
-
-const UpdateAudioDurationPage = () => <UpdateAudioDuration />;
-
-export default UpdateAudioDurationPage;
